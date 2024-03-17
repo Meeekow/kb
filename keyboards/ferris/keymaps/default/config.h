9cca16ed996ab74f33397374d1946a4d5210ca9f
Meeekow <68207769+Meeekow@users.noreply.github.com>
2024-03-17 16:58:01 +0800

    updated

--- keyboards/ferris/keymaps/default/config.h
+++ keyboards/ferris/keymaps/default/config.h
@@ -67,7 +67,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define FLOW_LAYERS_COUNT 2
 
 // How quickly all combo keys must be pressed in succession to trigger
-#define COMBO_TERM 25
+#define COMBO_TERM 50
 
 // Combo keys are always checked from layer 0, even if other layers are active
 #define COMBO_ONLY_FROM_LAYER 0
