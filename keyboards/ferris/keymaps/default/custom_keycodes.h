2714bc15f5026b92f944686d7fde8b0b3e9b2fc4
Meeekow <68207769+Meeekow@users.noreply.github.com>
2024-03-17 15:47:38 +0800

    cleanup

--- keyboards/ferris/keymaps/default/custom_keycodes.h
+++ keyboards/ferris/keymaps/default/custom_keycodes.h
@@ -6,15 +6,11 @@ enum keycodes {
    REPEAT = SAFE_RANGE,
    OS_UTL ,
    OS_SYM ,
-   OS_EXT ,
    SL_NUMO,
    SL_NUMX,
-   SL_TWMO,
-   SL_TWMX,
-   CDDIR  ,
-   ARWFUNC,
    COLON  ,
-   CSDEL  ,
+   ALTAB  ,
+   CIW    ,
    PANIC  ,
 };
 
@@ -24,8 +20,6 @@ enum layers {
     _UTL,
     _SYM,
     _NUM,
-    _TWM,
-    _EXT,
 };
 
 
@@ -33,7 +27,6 @@ enum layers {
 #define CTL_A C(KC_A)
 #define CTL_C C(KC_C)
 #define CTL_I C(KC_I)
-#define CTL_L C(KC_L)
 #define CTL_R C(KC_R)
 #define CTL_S C(KC_S)
 #define CTL_V C(KC_V)
@@ -42,31 +35,6 @@ enum layers {
 #define CTL_BS C(KC_BSPC)
 
 // TAB
+#define ALTAB   C(A(KC_TAB))
 #define TAB_BCK C(S(KC_TAB))
 #define TAB_FWD C(KC_TAB)
-
-// TWM
-#define TWM_S1 G(KC_1)
-#define TWM_S2 G(KC_2)
-#define TWM_S3 G(KC_3)
-#define TWM_S4 G(KC_4)
-#define TWM_S5 G(KC_5)
-#define TWM_S6 G(KC_6)
-#define TWM_S7 G(KC_7)
-#define TWM_S8 G(KC_8)
-#define TWM_S9 G(KC_9)
-
-#define TWM_TER G(S(KC_ENT))
-#define TWM_RET G(KC_ENT)
-#define TWM_RUN G(KC_P)
-
-#define TWM_H G(KC_H)
-#define TWM_J G(KC_J)
-#define TWM_K G(KC_K)
-#define TWM_L G(KC_L)
-
-#define TWM_C G(S(KC_C))
-
-#define TWM_SSQ G(S(KC_Q))
-#define TWM_SCSQ G(C(S(KC_Q)))
-
