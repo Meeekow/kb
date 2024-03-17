6a4ed8a7aeb7763323fd7fa2612f49a54bbc1f78
Meeekow <68207769+Meeekow@users.noreply.github.com>
2024-03-17 15:50:02 +0800

    cleanup

--- keyboards/ferris/keymaps/default/smart_layer.h
+++ keyboards/ferris/keymaps/default/smart_layer.h
@@ -13,17 +13,5 @@ void num_mode_disable(void);
 void num_mode_process(uint16_t keycode, keyrecord_t *record);
 
 
-/* -------- TWM Mode -------- */
-// Turn twm mode on. To be called from a custom keycode
-void twm_mode_enable(keyrecord_t *record);
-
-// Turn twm mode off.
-void twm_mode_disable(void);
-
-// Handle each key for twm mode.
-void twm_mode_process(uint16_t keycode, keyrecord_t *record);
-
-
 /* -------- Process Record -------- */
 void process_layermodes(uint16_t keycode, keyrecord_t *record);
-
