1cc480a47b14d46c7fe65c2fb786ffa5581ed77b
Meeko <mynameismeeko@gmail.com>
2024-07-25 06:01:54 +0800

    cleanup

--- keyboards/ferris/keymaps/default/smart_layer.c
+++ keyboards/ferris/keymaps/default/smart_layer.c
@@ -5,21 +5,22 @@
 /* -------- Number Mode -------- */
 static bool _num_mode_active = false;
 static uint16_t num_mode_timer;
-// Turn number mode on. To be called from a custom keycode
+// Turn number mode on. To be called from a custom keycode.
 void num_mode_enable(keyrecord_t *record) {
     if (record->event.pressed) {
         layer_on(_NUM);
         num_mode_timer = timer_read();
     } else {
         if (timer_elapsed(num_mode_timer) < TAPPING_TERM) {
-            // Tapping enables layer mode
+            // Tapping enables layer mode.
             _num_mode_active = true;
         } else {
-            // Holding treats as a normal LT
+            // Holding treats as a normal LT.
             layer_off(_NUM);
         }
     }
 }
+
 
 // Turn number mode off.
 void num_mode_disable(void) {
@@ -27,13 +28,15 @@ void num_mode_disable(void) {
     layer_off(_NUM);
 }
 
+
+// Assess if we should exit layermode or continue processing normally.
 void num_mode_process(uint16_t keycode, keyrecord_t *record) {
-    // Assess if we should exit layermode or continue processing normally.
+    // Define what keys will exit num mode.
     switch (keycode) {
         case KC_LCTL ... KC_LGUI:
         case KC_1 ... KC_0:
         case KC_BSPC:
-            // process the code and stay in the mode *dabs*
+            // Process the code and stay in the mode *dabs*
             break;
         default:
             // All other keys disable the layer mode on keyup.
