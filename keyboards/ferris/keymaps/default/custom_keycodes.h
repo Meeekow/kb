a72688c4aeedc94eb98596312caa214dbfad4e25
Meeko <mynameismeeko@gmail.com>
2024-11-01 09:12:20 +0800

    updated smart layer

--- keyboards/ferris/keymaps/default/custom_keycodes.h
+++ keyboards/ferris/keymaps/default/custom_keycodes.h
@@ -25,11 +25,11 @@ enum layers {
 #define CTL_A C(KC_A)
 #define CTL_C C(KC_C)
 #define CTL_I C(KC_I)
+#define CTL_L C(KC_L)
 #define CTL_R C(KC_R)
 #define CTL_S C(KC_S)
 #define CTL_V C(KC_V)
 #define CTL_W C(KC_W)
-#define CTL_X C(KC_X)
 #define CTL_Z C(KC_Z)
 #define CTL_BS C(KC_BSPC)
 
--- keyboards/ferris/keymaps/default/keymap.c
+++ keyboards/ferris/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       OS_UTL, KC_SPC, KC_ESC, OS_SYM),
 
    [_UTL] = LAYOUT_split_3x5_2(
-      CTL_R  , CTL_W  , TAB_BCK, TAB_FWD, CTL_X ,        KC_HOME, KC_PGDN, KC_PGUP, KC_END , KC_DEL,
+      CTL_R  , CTL_W  , TAB_BCK, TAB_FWD, CTL_L ,        KC_HOME, KC_PGDN, KC_PGUP, KC_END , KC_DEL,
       KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, GUIALT,        KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, REPEAT,
       CTL_Z  , CTL_A  , CTL_C  , CTL_V  , CTL_S ,        CTL_I  , CTL_BS , KC_BSPC, KC_TAB , PANIC ,
       KC_TRNS, CW_TOGG, KC_ENT, SL_NUMO),
